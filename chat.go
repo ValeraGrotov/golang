634d6e48e5af43ef9e2bc6ccf714500a543ecfb8
ValeraGrotov <valeragrotov@gmail.com>
2020-11-06 20:00:32 +0300

    Update chat.go

--- chat.go
+++ chat.go
@@ -1,45 +1,28 @@
 package main
-
 import (
 	"bufio"
-	"fmt"
-	"os"
 	"strings"
 	"github.com/go-redis/redis"
 )
-
 func main() {	
-	redisdb := redis.NewClient(&redis.Options{
-		Addr:     "addr:6379",
-	})
+	redisdb := redis.NewClient(&redis.Options{ Addr: "addr:6379" })
 	pubsub := redisdb.Subscribe("chat")
 	_, err := pubsub.Receive()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
+	if err != nil { println(err); return }
 	ch := pubsub.Channel()
-  
 	go func() {
 		for msg := range ch {
 			if !strings.Contains(msg.Payload, "2:BIG") {
 				array := strings.Split(msg.Payload, ":BIG")
-				message := array[1]
-				fmt.Println(message)
+				println(array[1])
 			}
 		}
 	}()
-
 	for {
 		var message string
 		scanner := bufio.NewScanner(os.Stdin)
-		if scanner.Scan() {
-			message = scanner.Text()
-		}
+		if scanner.Scan() { message = scanner.Text() }
 		err = redisdb.Publish("chat", "2:BIG"+message).Err()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
-		}
+		if err != nil { println(err); return }
 	}
 }
